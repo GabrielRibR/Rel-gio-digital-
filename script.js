7cab6f54a758a48ab72f2e62dbedda03ad9b4a8f
GabrielRibR <00001124003046sp@al.educacao.sp.gov.br>
2024-08-30 15:02:42 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function atualizarRelogio() {
+    const agora = new Date();
+    const horas = String(agora.getHours()).padStart(2, '0');
+    const minutos = String(agora.getMinutes()).padStart(2, '0');
+    const segundos = String(agora.getSeconds()).padStart(2, '0');
+
+    document.getElementById('horas').textContent = horas;
+    document.getElementById('minutos').textContent = minutos;
+    document.getElementById('secundos').textContent = segundos;
+}
+
+setInterval(atualizarRelogio, 1000);
+atualizarRelogio();  // Para inicializar imediatamente
